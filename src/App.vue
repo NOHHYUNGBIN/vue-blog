75fa4ef60f9066d35f5aa706ee44486334b1295d
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-12 23:00:34 +0900

    initial data

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import List from "@/components/List.vue";
+import { ref } from "vue";
+import initData from "./assets/blog";
+const data = ref(initData);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </header>
+  </nav>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h5>테스트로 만드는 페이지입니다.</h5>
+  </div>
+  <List :blogData="data" />
 </template>
 
 <style scoped>
---
+++ src/components/List.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  blogData: Array,
+});
+</script>
+
+<template>
+  <div v-for="data in blogData" :key="data.number">
+    <h5>{{ data.title }}</h5>
+    <p>{{ data.date }}</p>
+  </div>
+</template>
