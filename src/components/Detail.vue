5f93704d7efd2173edc0fad3599e72c26bb950d8
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-13 22:19:18 +0900

    dynamic route

---
+++ src/components/Detail.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  blogData: Array,
+});
+</script>
+
+<template>
+  <div>
+    <h4>상세페이지</h4>
+    <h5>{{ blogData[$route.params.id].title }}</h5>
+    <p>{{ blogData[$route.params.id].content }}</p>
+  </div>
+</template>
